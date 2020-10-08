09b31514cc6e6107504f2f551b28a2f2910a3dd4
Luke Chu <37006668+lukechu10@users.noreply.github.com>
2020-10-07 21:38:23 -0700

    first commit

---
+++ src/components/App.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+import Copywright from "./Copywright.svelte";
+
+</script>
+
+<div id="app">
+	<section class="todoapp">
+		
+	</section>
+	<Copywright />
+</div>
---
+++ src/components/Copywright.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+</script>
+
+<footer class="info">
+    <p>Double-click to edit a todo</p>
+</footer>
